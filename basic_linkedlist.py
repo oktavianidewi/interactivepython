4fc70b99d9d573b6a188831f6e12232e62d4b168
oktavianidewi <oktaviani.dewi@gmail.com>
2016-02-01 18:03:02 +0900

    linked list

--- basic_linkedlist.py
+++ basic_linkedlist.py
@@ -19,9 +19,63 @@ class node:
     def setNext(self, newnext):
         self.next = newnext
 
+class unorderedlist:
+    def __init__(self):
+        self.head = None
+
     def isEmpty(self):
         return self.head == None
 
+    def add(self, item):
+        temp = node(item)
+        # menyimpan info pointer selanjutnya adalah old head
+        temp.setNext(self.head)
+        #  mengisi nilai head baru dengan nilai item
+        self.head = temp
 
-temp = node(93)
-print temp.getData()
+    def size(self):
+        current = self.head
+        count = 0
+        while current!=None:
+            count += 1
+            current = current.getNext()
+        return count
+
+    def search(self, keyword):
+        current = self.head
+        count = 0
+        found = False
+        while found == False and current != None:
+            if current.getData() == keyword:
+                found = True
+            else:
+                count += 1
+                current = current.getNext()
+        return found
+
+    def remove(self, keyword):
+        current = self.head
+        count = 0
+        found = False
+        while found == False:
+            if current.getData() == keyword:
+                found = True
+            else:
+                previous = current
+                current = current.getNext()
+        if previous == None:
+            self.head = current.getNext
+        else:
+            previous.setNext(current.getNext())
+
+mylist = unorderedlist()
+mylist.add(0)
+mylist.add(1)
+mylist.add(2)
+mylist.add(3)
+mylist.add(4)
+mylist.add(5)
+print(mylist.size())
+print(mylist.search(2))
+print(mylist.remove(4))
+print(mylist.size())
