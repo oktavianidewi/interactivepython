ffead01f69fae2ad0ba61ef6a5b802bcd5598c21
oktavianidewi <oktaviani.dewi@gmail.com>
2016-02-03 21:52:07 +0900

    recursive_convertstring.py

--- recursive_convertstring.py
+++ recursive_convertstring.py
@@ -1,5 +1,7 @@
 import math
+from stack import stack
 
+# without stack
 def toStr(n, base):
     convertString = "0123456789ABCDEF"
     if n < base:
@@ -7,4 +9,22 @@ def toStr(n, base):
     else:
         return toStr(n//base, base) + convertString[n % base]
 
-print(toStr(1453, 6))
+# with stack
+rStack = stack()
+def toStrStack(n, base):
+    convertString = "0123456789ABCDEF"
+    while n > 0:
+        if n < base:
+            rStack.push(convertString[n])
+        else:
+            rStack.push(convertString[n % base])
+        n = n//base
+    res = ""
+
+    # ketika tidak kosong
+    while not rStack.isEmpty():
+        res = res + str(rStack.pop())
+    return res
+
+# print(toStr(1453, 6))
+print(toStrStack(1453, 6))
